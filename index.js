065f57a874c310a91f5e154e365d73e5d8d4b772
JiyaGupta-cs <140608790+JiyaGupta-cs@users.noreply.github.com>
2024-07-23 00:44:41 +0530

    Fixed MongoURL

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = 8000;
-mongoose.connect("mongodb://localhost:27017/hanashi").then(e=>console.log("MongoDB connected"));
+
+let MONGO_URL;
+MONGO_URL =  "mongodb+srv://coderaholic21:8fSVxvADmFp62Wv4@cluster0.eea8kg6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+ 
+
+const PORT = process.env.PORT || 8000;
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 const Blog = require("./models/blog");
-app.set("view engine","ejs");
-app.set("views",path.resolve("./views"))
-app.use(express.urlencoded({extended:false}))
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve('./public')));
-app.get('/',async(req,res)=>{
-    const allBlogs = await Blog.find({});
-    res.render("home",{
-        user:req.user,
-        blogs:allBlogs,
-    });
-})
-app.use("/user",userRoute);
-app.use("/blog",blogRoute);
 
-app.listen(PORT,()=>{console.log(`Server Started at Port:${PORT}`)});
+app.get('/', async (req, res) => {
+    try {
+        const allBlogs = await Blog.find({});
+        res.render("home", {
+            user: req.user,
+            blogs: allBlogs,
+        });
+    } catch (error) {
+        console.error("Error fetching blogs:", error);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server Started at Port: ${PORT}`);
+});
